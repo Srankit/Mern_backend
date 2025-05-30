73ecdf46d932591ef44d9ab4353941167b25bd80
ankit <ankitsarar142@gmail.com>
2025-05-30 17:37:19 +0530

    utils file add

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import { app } from "../app.js";
 
 const connectDb = async() => {
 try{
 const connectionInstance =  await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
 console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host} \n`);
+app.on("error", (error) => {    
+console.log("MongoDB connection error:", error);
+});
 }catch(error){
 console.error("Error connecting to MongoDB:", error);
 process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,236 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv';
 import connectDb from './db/connection.js';
+import { app } from './app.js';
 
 dotenv.config({ path: './env' });
 
-connectDb();
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT ||8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((error) => {
+  console.error('Error connecting to the database:', error);
+  process.exit(1);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import express from "express";
+
+const app = express();
+
+(async ()=>{
+try{
+await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+app.on("error",(error)=>{
+    console.log("MongoDB connection error:", error);
+    throw error;
+})
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+});
+}catch(error){
+console.error("Error connecting to MongoDB:", error);
+throw error;
+}
+})()
+
+*/
